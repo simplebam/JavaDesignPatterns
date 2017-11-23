b192cc367413768f5e4f9d7c1b36a53a695818b7
simplebam <3421448373@qq.com>
2017-11-23 12:13:13 +0800

    创建了状态模式的定义

--- src/observer/delegation1/Test.java
+++ src/observer/delegation1/Test.java
@@ -1,15 +1,13 @@
 package observer.delegation1;
 
-import java.lang.reflect.InvocationTargetException;
-
 /**
- *         作者：夏广成
+ 作者：夏广成
  链接：http://www.jianshu.com/p/c067410d95ca
  來源：简书
  著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class Test {
-    public static void main(String... args) throws NoSuchFieldException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static void main(String... args) {
 
         //委托 观察者
         Notifier notifier=new GoodNofitier();
