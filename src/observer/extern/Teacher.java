90be24b545873ee506028db5280f9fbadcb6f17a
simplebam <3421448373@qq.com>
2017-12-11 16:20:55 +0800

    设计模式初步完成

--- src/observer/extern/Teacher.java
+++ src/observer/extern/Teacher.java
@@ -7,7 +7,6 @@ public class Teacher extends Observable {
     //布置作业的状态信息字符串
     private String info;
     public void setHomework(String info) {
-
         this.info=info;
         System.out.println("布置的作业是"+info);
 
